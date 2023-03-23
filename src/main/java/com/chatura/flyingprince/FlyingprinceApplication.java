b8882cb57ae6792827aaeaaeff580c93c1def313
cdedigamuwa <chaturathilanka.dedigamuwa@yahooinc.com>
2023-03-23 19:11:19 +0100

    init commit

---
+++ src/main/java/com/chatura/flyingprince/FlyingprinceApplication.java
@@ -0,0 +1,13 @@
+package com.chatura.flyingprince;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingprinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingprinceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatura/flyingprince/FlyingprinceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.chatura.flyingprince;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FlyingprinceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
